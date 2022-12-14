2b7bb5c823c3c947a3788b8efc40b926cc9bac1a
MadanMohanReddy <madan.patakota@hotmail.com>
2022-12-14 09:02:00 +0530

    angular forms tdd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngualrForms-BatchFive';
+
+
+
+
+  // Here i am getting form element
+  // but i want the type of the form i mean 
+  // i want to know form related status
+  // i mean i want to know each status of the control
+  evtSubmit(form:NgForm){
+
+    console.log(form);
+  }
+
+
 }
